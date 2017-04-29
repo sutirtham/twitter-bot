08abbe3a2ed87fe831bfd3478b1550a4b689610b
sutirtham <sutirtha.malakar1@gmail.com>
2017-04-30 03:34:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-consumer_key = "727bQM1xCkWAkUK71eyiLYswB"
-consumer_secret = "f59ft7bIG8ody2nMxh2ar004EF7DoweYkJVepzh4dKCypkWyQS"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "857144293298974722-HGxUEKWRNsLR59dYhixJ9LXQhpf7JZi"
-access_token_secret = "QKxSddYxYXVQ1k9PW3w9cjxnDoONaERiQXawNoS6fXhLg"
+access_token = ""
+access_token_secret = ""
