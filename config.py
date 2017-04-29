dfbceda2378330a4973e092605d26335f5459d8f
sutirtham <sutirtha.malakar1@gmail.com>
2017-04-30 03:10:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-consumer_key = "Your consumer key"
-consumer_secret = "Your consumer secret"
+consumer_key = "727bQM1xCkWAkUK71eyiLYswB"
+consumer_secret = "f59ft7bIG8ody2nMxh2ar004EF7DoweYkJVepzh4dKCypkWyQS"
 
-access_token = "Your access token"
-access_token_secret = "Your access token secret"
+access_token = "857144293298974722-HGxUEKWRNsLR59dYhixJ9LXQhpf7JZi"
+access_token_secret = "QKxSddYxYXVQ1k9PW3w9cjxnDoONaERiQXawNoS6fXhLg"
